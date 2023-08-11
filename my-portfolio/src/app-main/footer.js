95c754960b0e4b66cd75476718071d95663fcf27
robpap96 <robertopapagni1996@outlook.it>
2023-08-11 21:03:25 +0200

    structure added

---
+++ my-portfolio/src/app-main/footer.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+        {/* Contenuto dell'footer */}
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ my-portfolio/src/app-main/main.js
@@ -0,0 +1,21 @@
+// Header.js
+import React from 'react';
+
+import About from './components/About';
+import Technologies from './components/Technologies';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+
+const Main = () => {
+    return (
+        <main>
+            <About />
+            <Technologies />
+            <Projects />
+            <Contact />
+        </main>
+    );
+}
+
+export default Main;
