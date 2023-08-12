ad949b22a0b3035e498c2e35ce50257d5f134619
robpap96 <robertopapagni1996@outlook.it>
2023-08-12 18:40:23 +0200

    boh

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 
+import { ChakraProvider,extendBaseTheme } from '@chakra-ui/react'
+import chakraTheme from '@chakra-ui/theme'
+
 import Header from './app-main/Header';
 import Main from './app-main/Main';
 import Footer from './app-main/Footer';
 
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <div className="App">
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        </ChakraProvider> 
+    );
 }
 
 export default App;
--- my-portfolio/src/app-main/header.js
+++ my-portfolio/src/app-main/header.js
@@ -1,10 +1,27 @@
 // Header.js
 import React from 'react';
+//chakra components
+import { Button, Flex } from '@chakra-ui/react';
+
 
 const Header = () => {
     return (
         <header>
-            
+            <Flex align="center" justify="between">
+                <Flex align="center" justify="">
+                    <Flex >
+                        <Button>prova</Button>
+                        <Button>prova</Button>
+                    </Flex>
+                </Flex>
+                <Flex align="center" justify="">
+                    <Flex >
+                        <Button>prova</Button>
+                        <Button>prova</Button>
+                    </Flex>
+                </Flex>
+
+            </Flex>
         </header>
     );
 }
