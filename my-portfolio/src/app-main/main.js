ae6f20097e57ef85678f63c8154ff8909347ee5f
robpap96 <robertopapagni1996@outlook.it>
2023-08-12 17:13:39 +0200

    micro fix

--- my-portfolio/src/app-main/main.js
+++ my-portfolio/src/app-main/main.js
@@ -4,7 +4,7 @@ import React from 'react';
 import About from './components/About';
 import Technologies from './components/Technologies';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Contact from './components/Contacts';
 
 
 const Main = () => {
